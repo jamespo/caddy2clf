2cb6f406d7dc4bc2f6d07cc2c910b3de18b06ccd
James Powell <jamespo@gmail.com>
2024-05-01 16:04:59 +0100

    forgot modified test file

--- test_caddy2clf.py
+++ test_caddy2clf.py
@@ -2,33 +2,35 @@
 
 import tempfile
 import unittest
-from unittest.mock import patch, mock_open
+from unittest.mock import patch
 from io import StringIO
-from caddy2clf import process_logs, open_pipe, format_line
+from caddy2clf import process_pipe
 import os
 
 class TestCaddy2Clf(unittest.TestCase):
 
     def setUp(self):
-        self.outfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
-        self.outfile.close()
+        # self.outfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        # self.outfile.close()
         self.infile = tempfile.NamedTemporaryFile(mode='w', delete=False)
-        self.infile.write('{"level":"info","ts":1713985562.6151454,"logger":"http.log.access.log0","msg":"handled request","request":{"remote_ip":"120.77.35.242","remote_port":"50538","client_ip":"120.77.35.242","proto":"HTTP/1.1","method":"GET","host":"armyman.fancydan.co.uk","uri":"/wp-login.php","headers":{"User-Agent":["Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/95.0"],"Accept-Encoding":["gzip"],"Connection":["close"]}},"bytes_read":0,"user_id":"","duration":0.0000418,"size":0,"status":308,"resp_headers":{"Server":["Caddy"],"Connection":["close"],"Location":["https://www.bbc.co.uk/"],"Content-Type":[]}}\n')
+        self.infile.write('{"level":"info","ts":1713985562.6151454,"logger":"http.log.access.log0","msg":"handled request","request":{"remote_ip":"120.77.35.242","remote_port":"50538","client_ip":"120.77.35.242","proto":"HTTP/1.1","method":"GET","host":"www.bbc.co.uk","uri":"/wp-login.php","headers":{"User-Agent":["Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/95.0"],"Accept-Encoding":["gzip"],"Connection":["close"]}},"bytes_read":0,"user_id":"","duration":0.0000418,"size":0,"status":308,"resp_headers":{"Server":["Caddy"],"Connection":["close"],"Location":["https://www.bbc.co.uk/"],"Content-Type":[]}}\n')
         self.infile.close()
 
     def tearDown(self):
         #self.outfile.close()
         #self.infile.close()
         try:
-            os.remove(self.outfile.name)
+            # os.remove(self.outfile.name)
             os.remove(self.infile.name)
         except:
             pass
 
-    def test_process_logs_pipe(self):
-        input_pipe = open_pipe('cat %s' % self.infile.name)
-        process_logs(input_pipe.stdout, self.outfile.name)
-                        
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_process_logs_pipe(self, mock_stdout):
+        process_pipe('cat %s' % self.infile.name, None)
+        self.assertEqual(mock_stdout.getvalue(), '120.77.35.242 - - 24/Apr/2024:20:06:02  "GET /wp-login.php HTTP/1.1" 308 0\n')
+
+
 
 if __name__ == '__main__':
     unittest.main()
