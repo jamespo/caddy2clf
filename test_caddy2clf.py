1046f71230bf4235aaf55d97883bfbd440475931
James Powell <jamespo@gmail.com>
2024-05-04 14:33:01 +0100

    cleanup

--- test_caddy2clf.py
+++ test_caddy2clf.py
@@ -10,17 +10,12 @@ import os
 class TestCaddy2Clf(unittest.TestCase):
 
     def setUp(self):
-        # self.outfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
-        # self.outfile.close()
         self.infile = tempfile.NamedTemporaryFile(mode='w', delete=False)
         self.infile.write('{"level":"info","ts":1713985562.6151454,"logger":"http.log.access.log0","msg":"handled request","request":{"remote_ip":"120.77.35.242","remote_port":"50538","client_ip":"120.77.35.242","proto":"HTTP/1.1","method":"GET","host":"www.bbc.co.uk","uri":"/wp-login.php","headers":{"User-Agent":["Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/95.0"],"Accept-Encoding":["gzip"],"Connection":["close"]}},"bytes_read":0,"user_id":"","duration":0.0000418,"size":0,"status":308,"resp_headers":{"Server":["Caddy"],"Connection":["close"],"Location":["https://www.bbc.co.uk/"],"Content-Type":[]}}\n')
         self.infile.close()
 
     def tearDown(self):
-        #self.outfile.close()
-        #self.infile.close()
         try:
-            # os.remove(self.outfile.name)
             os.remove(self.infile.name)
         except:
             pass
